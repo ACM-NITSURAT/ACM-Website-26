985e3a94374c5f82dfcf85d149477bcafcef5ce2
Aayush Prasad <aayushp336@gmail.com>
2026-06-14 14:05:44 +0530

    chore(auth): add helpers for authentication

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -1,3 +1,12 @@
 export { default as app } from './app';
 export { default as db } from './firestore';
 export { getAnalyticsInstance } from './analytics';
+export {
+  auth,
+  signInWithGoogle,
+  registerWithEmail,
+  loginWithEmail,
+  sendVerificationEmail,
+  sendPasswordReset,
+  logout,
+} from './auth';
